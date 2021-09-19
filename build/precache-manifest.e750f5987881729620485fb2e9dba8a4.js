16d1cb30193dbe279b130422f8b049af4fab99a9
mcbobby <madumcbobby@yahoo.com>
2021-09-19 14:29:12 +0100

    fix error

---
+++ build/precache-manifest.e750f5987881729620485fb2e9dba8a4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbaf09605bd30e7359afc0d4e1daffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0333ab5ab7ce6172e81b",
+    "url": "/static/css/2.21158041.chunk.css"
+  },
+  {
+    "revision": "099cb4070f53da66b81d",
+    "url": "/static/css/main.3c508f50.chunk.css"
+  },
+  {
+    "revision": "0333ab5ab7ce6172e81b",
+    "url": "/static/js/2.d190e9cc.chunk.js"
+  },
+  {
+    "revision": "e9c6dc1adf2dce518ecd97d3231fb67a",
+    "url": "/static/js/2.d190e9cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099cb4070f53da66b81d",
+    "url": "/static/js/main.f75e154d.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  },
+  {
+    "revision": "ee0c4138e1bc8f0ca111160b24eff971",
+    "url": "/static/media/1.ee0c4138.png"
+  },
+  {
+    "revision": "2f3a42c0b4adf15f0e94581067639d92",
+    "url": "/static/media/banner-bg.2f3a42c0.jpg"
+  },
+  {
+    "revision": "5231214d2c8f1ed605eb63786b50e66e",
+    "url": "/static/media/error.5231214d.gif"
+  },
+  {
+    "revision": "4d768680e573bd4fbd1c897a9152d66c",
+    "url": "/static/media/error2.4d768680.svg"
+  },
+  {
+    "revision": "2980083682e94d33a66eef2e7d612519",
+    "url": "/static/media/fontawesome-webfont.29800836.svg"
+  },
+  {
+    "revision": "706450d7bba6374ca02fe167d86685cb",
+    "url": "/static/media/fontawesome-webfont.706450d7.ttf"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "d9ee23d59d0e0e727b51368b458a0bff",
+    "url": "/static/media/fontawesome-webfont.d9ee23d5.woff"
+  },
+  {
+    "revision": "f7c2b4b747b1a225eb8dee034134a1b0",
+    "url": "/static/media/fontawesome-webfont.f7c2b4b7.eot"
+  },
+  {
+    "revision": "ed3c26ccdfbfba024982a3f56c5e5375",
+    "url": "/static/media/logoBlue.ed3c26cc.png"
+  },
+  {
+    "revision": "a36d2ca30cf8536fbb14ea1cb491e97e",
+    "url": "/static/media/logoWhite.a36d2ca3.png"
+  },
+  {
+    "revision": "e500d35a3b6679804ec039fce7495fea",
+    "url": "/static/media/success.e500d35a.gif"
+  },
+  {
+    "revision": "4eb6403d14e44f3170f78cfdedb15875",
+    "url": "/static/media/success2.4eb6403d.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "/build")));
   app.get("*", (req, res) =>
     res.sendFile(
       path.join(__dirname, "build/index.html")
